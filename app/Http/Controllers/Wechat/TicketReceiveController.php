d1c423f1c083fcc2ce5e8ddc2238fdb28c3f2852
滕明志 <george@ou.cc>
2024-10-09 18:11:49 +0800

    wechat verify file.

--- app/Http/Controllers/Wechat/TicketReceiveController.php
+++ app/Http/Controllers/Wechat/TicketReceiveController.php
@@ -8,12 +8,21 @@ use Illuminate\Support\Facades\Log;
 
 class TicketReceiveController extends Controller
 {
+    private string $token = 'c3kGs6nYUQGpUGGqiKPe6VpELB70gT9UC9MJqv56';
+
     public function receive(Request $request): void
     {
-        $data = $request->all();
-        $body = $request->getContent();
-        Log::info(json_encode($data));
-        Log::info($body);
-        echo 'success';
+        $params = $request->all();
+        $bodyXml = $request->getContent();
+        $bodyObj = simplexml_load_string($bodyXml,'SimpleXMLElement', LIBXML_NOCDATA);
+        $bodyObj = json_encode($bodyObj);
+        $bodyArr = json_decode($bodyObj,true);
+        $verify = [$params['timestamp'], $params['nonce'], $this->token, $bodyArr['Encrypt']];
+        natcasesort($verify);
+//        var_dump($verify);
+//        var_dump(implode($verify));
+        $verify = sha1(implode($verify));
+
+        echo $verify;
     }
 }
